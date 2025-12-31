8cfbe953000c3505295ce928167ad00b65166876
Akash Nagar <aknagar@microsoft.com>
2025-12-31 21:00:37 +0530

    refactor: rename AugmentService to AugmentService.Api
    - Rename folder from AugmentService to AugmentService.Api
    - Rename AugmentService.csproj to AugmentService.Api.csproj
    - Update AppHost project reference
    - Update AppHost Program.cs to use Projects.AugmentService_Api
    - Update solution file
    - Clean build: 0 errors, 0 warnings

--- backend/MfePortal.AppHost/Program.cs
+++ backend/MfePortal.AppHost/Program.cs
@@ -1,5 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.AugmentService>("augmentservice");
+builder.AddProject<Projects.AugmentService_Api>("augmentservice");
 
 builder.Build().Run();
