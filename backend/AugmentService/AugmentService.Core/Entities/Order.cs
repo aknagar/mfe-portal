700ae1fb706c2a12c2b9b5b3cc79aae1d47598e4
aknagar <akashnagar47@outlook.com>
2026-01-13 21:59:47 +0530

    Clean up OrdersController and Order entity
    - Remove XML documentation comments from OrdersController
    - Remove commented-out Service Bus code
    - Remove unused GET endpoint
    - Remove unnecessary using statements from Order.cs

--- backend/AugmentService/AugmentService.Core/Entities/Order.cs
+++ backend/AugmentService/AugmentService.Core/Entities/Order.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace AugmentService.Core.Entities
 {
     public class Order
